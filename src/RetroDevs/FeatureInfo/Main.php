4a65a763276a6abb0ee7cb067b811ab118302951
Difz25 <166781931+Difz25@users.noreply.github.com>
2024-06-10 17:54:22 +0700

    first

---
+++ src/RetroDevs/FeatureInfo/Main.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RetroDevs\FeatureInfo;
+
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+
+    public Config $cfg;
+
+    public function onEnable(): void {
+        $this->saveResource("config.yml");
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "feature") {
+            if($sender instanceof Player) {
+                $this->FeatureUI($sender);
+            } else {
+                $sender->sendMessage("Please use this command in-game.");
+            }
+        }
+        return true;
+    }
+
+    public function FeatureUI(Player $player): void
+    {
+        $form = new SimpleForm(function (Player $player, $data) {
+            if ($data === null) {
+                return false;
+            }
+            return true;
+        });
+        $form->setTitle($this->cfg->get("feature-title"));
+        $form->setContent($this->cfg->get("feature-info"));
+        $player->sendForm($form);
+    }
+}
